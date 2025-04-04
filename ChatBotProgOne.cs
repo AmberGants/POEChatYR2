fb001215708bbc7d39ac5bbd63973cccfa81c71e
Ambesiwe Gantsho <162144636+AmberGants@users.noreply.github.com>
2025-04-04 15:35:30 +0000

    Initial commit
    Set up project structure and main files

---
+++ ChatBotProgOne.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Speech.Synthesis;
+using System.Linq;
+using System.Threading;
+
+class Program
+{
+    static SpeechService speechService = new SpeechService();
+
+    static void Main(string[] args)
+    {
+        
+    }
+}
